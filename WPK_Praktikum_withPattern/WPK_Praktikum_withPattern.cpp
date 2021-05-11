5d3ae3ad687fc9735be1a9725a6b316583cf6a14
Aryazaky <aryazakyazif@gmail.com>
2021-05-12 01:31:43 +0700

    Completed

--- WPK_Praktikum_withPattern/WPK_Praktikum_withPattern.cpp
+++ WPK_Praktikum_withPattern/WPK_Praktikum_withPattern.cpp
@@ -25,15 +25,18 @@ public:
         log.push_back(Message{ msg, isError });
     }
 };
+// A 2D double vector with assignable names
 class VectorD {
     vector<vector<double>> vec;
-    bool isValid(int index) {
+    vector<string> vec_names;
+    template<typename T>
+    bool isValid(vector<T> _vector, int index) {
         if (!isReady) {
             cout << "Vector is empty!" << endl;
             return false;
         }
-        if (index >= vec.size()) {
-            cout << "Out of range. Max index was " << vec.size() << ". index given = " << index << endl;
+        if (index >= _vector.size()) {
+            cout << "Out of range. Max index was " << _vector.size() << ". index given = " << index << endl;
             return false;
         }
         return true;
@@ -58,29 +61,46 @@ public:
     }
     // Returns a copy of the whole 2D vector
     const vector<vector<double>> Get() { return vec; }
+    int GetSize() { return vec.size(); }
+    int GetSize(int index) { return vec[index].size(); }
     void Push(double x, int index) {
-        if (isValid(index)) {
+        if (isValid(vec, index)) {
             vec[index].push_back(x);
         }
     }
+    void Push(double x, int index, string new_name) {
+        if (isValid(vec, index)) {
+            vec[index].push_back(x);
+            SetVecName(index, new_name);
+        }
+    }
     // Returns a copy of a vector on the specified index
-    const vector<double> At(int index) {
-        if (isValid(index)) {
+    const vector<double> Get(int index) {
+        if (isValid(vec, index)) {
             return vec[index];
         }
     }
     void AddEmptyVec(int count) {
         while (count--) {
             vec.push_back(vector<double>());
+            vec_names.push_back("vec" + GetSize());
             isReady = true;
         }
     }
+    void SetVecName(int index, string new_name) {
+        if (isValid(vec_names, index)) {
+            vec_names[index] = new_name;
+        }
+    }
     void PopBackVec(int count) {
-        if (isValid(count - 1)) {
+        if (isValid(vec, count - 1)) {
             while (count--) {
                 vec.pop_back();
             }
         }
+    }
+    string GetVecName(int index) {
+        return vec_names[index];
     }
 };
 class Function {
@@ -91,57 +111,89 @@ class Method {
 protected:
     VectorD vec;
     Function* func;
+    bool isFilled;
+    virtual double CalculateDifference(double x, double delta_x) = 0;
+    virtual double ErrorValue(double x, double delta_x) {
+        return abs(CalculateDifference(x, delta_x) - MiddleDifference(x, delta_x));
+    }
+    double MiddleDifference(double x, double delta_x) {
+        return (func->Fx(x + delta_x) - func->Fx(x - delta_x)) / delta_x / 2;
+    }
 public:
-    Method(Function& function, int vec_size) : func(&function), vec(vec_size) { }
+    Method(Function& function, int vec_size) : func(&function), vec(vec_size), isFilled(false) { }
     void ChangeFunction(Function& new_function) { func = &new_function; }
-    virtual void run(double x) = 0;
-    virtual void run(double a, double b, int n) = 0;
-    virtual void run(double a, double b, int n, int m) = 0;
-    virtual void run(double a, double b, double e, int n) = 0;
+    virtual void PrintTable() {
+        if (!isFilled) {
+            cout << "[ERROR] Table not yet generated\n";
+            return;
+        }
+        cout << "\nResult Table:\n";
+        for (int i = 0; i < vec.GetSize(); i++) {
+            cout << vec.GetVecName(i) << "\t";
+        }
+        cout << endl;
+        for (int i = 0; i < vec.GetSize(0); i++) {
+            for (int o = 0; o < vec.GetSize(); o++) {
+                cout << vec.Get(o)[i] << "\t";
+            }
+        }
+        cout << endl;
+    }
+    virtual void run(double x) { cout << "[ERROR] Not implemented\n"; }
+    virtual void run(double a, double b, double h) { cout << "[ERROR] Not implemented\n"; }
+    virtual void run(double a, double b, int n) { cout << "[ERROR] Not implemented\n"; }
+    virtual void run(double a, double b, double h, int n) { cout << "[ERROR] Not implemented\n"; }
 };
 
 // -------------------------------------------------------------------
 // Fungsi dan method di bawah
 // -------------------------------------------------------------------
 
-class FX_expsin2Xp1 : public Function {
+// Fungsi A
+class FX_expmXsin2Xp1 : public Function {
 public:
     double Fx(double x) {
         return exp(-x) * sin(2 * x) + 1;
     }
 };
+// Turunan fungsi A
+class FX_expmX2cos2Xmsin2X : public Function {
+public:
+    double Fx(double x) {
+        return exp(-x) * (2 * cos(2 * x) - sin(2 * x));
+    }
+};
+// Turunan dari turunan fungsi A
+class FX_mexpmX3sin2Xp4cos2X : public Function {
+public:
+    double Fx(double x) {
+        return -exp(-x) * (3 * sin(2 * x) + 4 * cos(2 * x));
+    }
+};
 
 class ForwardNumericalDifferentiation : public Method {
-    void FillDifferentiationTable() {
-        for (int i = 2; i < vec.Get().size(); i++) {
-            for (int o = 1; o < vec.At(i - 1).size(); o++) {
-                vec.Push(vec.At(i - 1)[o] - vec.At(i - 1)[o - 1], i);
-            }
-        }
-    }
-    // Menghitung dari B hingga vector sebelum ini menggunakan rumus tertentu
-    void CalculatePn1(double h) {
-        vec.AddEmptyVec(1);
-        for (int j = 0; j < vec.At(0).size(); j++) {
-            double result = 0;
-            for (int i = 1; i < vec.Get().size() - 1; i++) {
-                if (i % 2 == 0) {
-                    result -= h / i * vec.At(i).at(j);
-                }
-                else {
-                    result += h / i * vec.At(i).at(j);
-                }
-            }
-        }
-        
+    double CalculateDifference(double x, double delta_x) {
+        return (func->Fx(x + delta_x) - func->Fx(x)) / delta_x;
     }
 public:
     ForwardNumericalDifferentiation(Function& function) : Method(function, 4) {}
     // Batas bawah = a
     // Batas atas = b
     // Pembagi = n
+    // Step = h
     // Index yang dihitung angkanya
-    void run(double a, double b, int n, int m) {
+    void run(double a, double b, double h) {
+        if (b < a) {
+            cout << "B must be larger than A\n";
+            return;
+        }
+        if (h <= 0) {
+            cout << "H must be more than 0\n";
+            return;
+        }
+        run(a, b, floor((b - a) / h));
+    }
+    void run(double a, double b, int n) {
         if (b < a) {
             cout << "B must be larger than A\n";
             return;
@@ -150,26 +202,37 @@ public:
             cout << "N must be at least 1\n";
             return;
         }
-        double h = (b - a) / n;
-        // Membuat tabel newton forward
 
-        // Ukuran vector A = n+1; isinya x yang sudah dibagi2 ke n+1 bagian
-        // Ukuran vector B = vector A; isinya hasil kalkulasi F(x) dari vector A dengan index yg sama
-        // Jumlah vector lain (B, C, D, dst) = n-1; isinya selisih dari vector sebelumnya index yang sama dengan index sebelumnya
+        double h = (b - a) / n;
+
         vec.Clear();
-        vec.Resize(2 + n - 1);
-        // Mengisi vector A dan B
+        vec.Resize(5);
+        // Isi tabel: 
+        // X; F(x); F'(x); F'eksak; error;
+
+        // Copy x, f(x), dan f'(x) ke tabel
         for (int i = 0; i < n + 1; i++) {
-            vec.Push(a + (h * i), 0); // 0 itu vector A
-            vec.Push(func->Fx(a + (h * i)), 1); // 1 itu vector B
+            double x = a + (h * i);
+            vec.Push(x, 0, "X"); // 0 itu vector X
+            vec.Push(func->Fx(x), 1, "F(X)"); // 1 itu vector F(X)
+            vec.Push(CalculateDifference(x, h), 2, "F'(X)");
+            vec.Push(MiddleDifference(x, h), 3, "F'eksak(X)");
+            vec.Push(ErrorValue(x, h), 4, "error");
+            isFilled = true;
         }
-        // Mengisi vector lainnya mulai dari C
-        FillDifferentiationTable();
-        // Menghitung forward differentiation
     }
 };
 
 int main()
 {
     cout << "Hello World!\n";
+    Function* fx;
+    fx = new FX_expmXsin2Xp1();
+    ForwardNumericalDifferentiation program(*fx);
+
+    program.PrintTable();
+    program.run(0, 1, 10);
+    program.PrintTable();
+
+    delete(fx);
 }
